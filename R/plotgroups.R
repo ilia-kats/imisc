fee3d0b9223845c1509bad2a90c439c65e1d8051
Ilia Kats <ilia-kats@gmx.net>
2016-09-22 17:44:31 +0200

    only omit Y ticks if cplot > 1

--- R/plotgroups.R
+++ R/plotgroups.R
@@ -996,7 +996,7 @@ plotgroups <- function(
             # tick also works for multiple plots
             ticks <- axTicks(side=2)
             lticks <- length(ticks)
-            if (ticks[lticks] > cylim[2] - 0.5 * lineheight)
+            if (cplot > 1 && ticks[lticks] > cylim[2] - 0.5 * lineheight)
                 ticks <- ticks[-lticks]
             do.call(axis, list.merge(pars, list(side=2, at=ticks)))
         }
