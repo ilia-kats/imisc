ca914a67c3ef465f8769e534ee758284b0f4c55d
Ilia Kats <ilia-kats@gmx.net>
2016-10-10 14:52:51 +0200

    only add spacing between data and significiance indicators if !ylim.extended

--- R/plotgroups.R
+++ R/plotgroups.R
@@ -953,7 +953,8 @@ plotgroups <- function(
             }
 
             signifmargin <- (maxsignifoverlaps + 1) * lineheight
-            legendmargin <- legendmargin + signifheight
+            if (!ylim.extended)
+                legendmargin <- legendmargin + signifheight
             if (is.null(legend.text))
                 legendmargin <- legendmargin + signifheight
         }
@@ -973,7 +974,9 @@ plotgroups <- function(
         if (!ylim.extended)
             legendbase <- legendbase + signifheight
         if (!is.null(signif.test[[cplot]])) {
-            signifbase <- legendbase + signifheight
+            signifbase <- legendbase
+            if (!ylim.extended)
+                signifbase <- signifbase + signifheight
             legendbase <- signifbase + signifmargin * inchestouser
         }
 
