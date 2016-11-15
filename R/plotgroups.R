3b232d35efd311b200507e53925db6c517b3cb1a
Ilia Kats <ilia-kats@gmx.net>
2016-11-15 11:15:37 +0100

    fix incorrect legendmargin for cplot > 1

--- R/plotgroups.R
+++ R/plotgroups.R
@@ -722,6 +722,8 @@ plotgroups <- function(
             } else {
                 legendmargin <- 0
             }
+        } else {
+            legendmargin <- 0
         }
         signifmargin <- 0
         if (!is.null(signif.test[[cplot]])) {
