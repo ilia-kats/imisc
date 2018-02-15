5eeaa21e38c54c2926787d181135a1d56f08b0c5
Ilia Kats <ilia-kats@gmx.net>
2018-02-15 19:03:00 +0100

    add a custom title function allowing for different positions of each title
    it is designed to be identical to graphics::title unless additional
    options or parameters are provided

---
+++ R/title.R
@@ -0,0 +1,69 @@
+#' Plot Annotation
+#'
+#' This function can be used to add labels to a plot. Its main difference to \code{\link[graphics]{title}}
+#' is that it allows to specify a different drawing position for each annotation. The defaults are the same
+#' as for \code{\link[graphics]{title}}, but can be changed by a call to \code{\link[base]{options}}.
+#'
+#' @param main Main plot title.
+#' @param sub Plot subtitle.
+#' @param xlab X axis label.
+#' @param ylab Y axis label.
+#' @param main.line Placement of the main title in lines. Defaults to \code{getOption('imisc.mgp.main')} or,
+#'                  if the option is not set, 3.
+#' @param sub.line Placement of the main title in lines. Defaults to \code{getOption('imisc.mgp.sub')} or,
+#'                  if the option is not set, \code{par('mgp')[1] + 1}.
+#' @param xlab.line Placement of the X axis label in lines. Defaults to \code{getOption('imisc.mgp.xlab')} or,
+#'                  if the option is not set, \code{par('mgp')[1]}.
+#' @param ylab.line Placement of the Y axis label in lines. Defaults to \code{getOption('imisc.mgp.xlab')} or,
+#'                  if the option is not set, \code{par('mgp')[1]}.
+#' @param use.default.line Use \code{\link[graphics]{title}} defaults instead of code{getOption('imisc.mgp')},
+#'                  even if the options are set.
+#' @param ... Additional parameters passed to \code{\link[graphics]{title}}
+#'
+#' @export
+#' @importFrom graphics par title
+
+title <- function(main=NULL,
+                  sub=NULL,
+                  xlab=NULL,
+                  ylab=NULL,
+                  main.line=NULL,
+                  sub.line=NULL,
+                  xlab.line=NULL,
+                  ylab.line=NULL,
+                  use.default.line=FALSE,
+                  ...)
+{
+    getline <- function(x) {
+        env <- parent.frame()
+        argname <- paste0(x, '.line')
+        if (is.null(env[[argname]])) {
+            globalmgp <- getOption(paste0('imisc.mgp.', x))
+            if (use.default.line || is.null(globalmgp)) {
+                if (x == 'main') {
+                    3
+                } else if (x == 'sub') {
+                    par('mgp')[1] + 1
+                } else {
+                    par('mgp')[1]
+                }
+            } else {
+                globalmgp
+            }
+        } else {
+            env[[argname]]
+        }
+    }
+
+    env <- environment()
+    for (t in c('main', 'sub', 'xlab', 'ylab')) {
+        if (!is.null(env[[t]])) {
+            args <- list(...)
+            args[[t]] <- env[[t]]
+            args$line <- getline(t)
+            do.call(graphics::title, args)
+        }
+    }
+    invisible()
+}
+
